a471db719af6c4c053f08441bdae6872f835fe24
kokily <kokily@naver.com>
2022-04-24 15:52:34 +0900

    Rebuild

--- src/api/upload/index.ts
+++ src/api/upload/index.ts
@@ -1,6 +1,9 @@
 import Router from 'koa-router';
 import authorized from '../../libs/middlewares/authorized';
+import image from './image';
 
 const upload = new Router();
+
+upload.post('/', authorized, image);
 
 export default upload;
